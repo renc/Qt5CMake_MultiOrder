9030b104bdd8b7e5253a787f00ac3d5483e454a1
canjiang <canjiang.ren@outlook.com>
2016-10-01 14:38:10 +0800

    Version 1: cmake + qt5 widget to create a mainwindow

---
+++ MainWindow.cpp
@@ -0,0 +1,12 @@
+﻿#include <QtWidgets>
+
+#include "MainWindow.h"
+
+MainWindow::MainWindow()
+{
+	QWidget *widget = new QWidget;
+	setCentralWidget(widget);
+
+	setWindowTitle(u8"多单程序"); 
+	setMinimumSize(640, 480);
+}
